e570500f7115c00b258ba7216a7935ad91c91e88
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-04-11 00:27:39 +0100

    merge conflits resolved

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "include/read_files.h"
 #include "include/knapsack.h"
@@ -7,20 +6,16 @@
 
 #include <chrono>
 using namespace std::chrono;
-=======
 #include "include/menu.h"
->>>>>>> 26a2f77fd3f7a58537750e2bbaf964782a0a272a
 
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
     // cen3();
     OptimizeProfit::greedyTrucksAndLinearKnapsack("../data/encomendas.txt", "../data/carrinhas.txt");
     OptimizeProfit::greedyTrucksAndFractionalKnapsack("../data/encomendas.txt", "../data/carrinhas.txt");
 
-=======
     Menu::start();
->>>>>>> 26a2f77fd3f7a58537750e2bbaf964782a0a272a
+
     return 0;
 }
